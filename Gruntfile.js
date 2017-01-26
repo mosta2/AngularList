c242b7c425c4bdbf253bf735559a5814cc7d064e
Mostafa <mosta_eg@hotmail.com>
2017-01-26 18:05:27 +0200

    j

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            combine: {
+                files: {
+                    'Content/main.css': ['Content/angular-material.min.css', 'Content/app.css']
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default',['cssmin']);
+};
